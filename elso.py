55fd56d425aaede4388497a26d401099de9788f2
Harman Patrik (SZF_2023_2025) <harman.patrik@diak.szamalk-szalezi.hu>
2024-02-01 15:17:29 +0100

    gyakorlas

---
+++ elso.py
@@ -0,0 +1,10 @@
+def elso():
+    VEGE = 0
+    db = 0
+    min = 2147483648
+    while ((szam := int(input("N=")) != VEGE:
+        if szam < min:
+            min = szam
+        db += 1
+
+    print(f"{min}")
